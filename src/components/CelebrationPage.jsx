a355b53389fc338cb7a607f3370ab4ae5b27defb
sarthakchauhan230-arch <sarthakchauhan230@gmail.com>
2026-01-18 04:48:08 +0530

    Update message text in CelebrationPage component

--- src/components/CelebrationPage.jsx
+++ src/components/CelebrationPage.jsx
@@ -389,7 +389,7 @@ function CelebrationPage({ onComplete, musicPlayerRef }) {
                   data-button="message"
                   onClick={() => handleButtonClick("message")}
                 >
-                  💌 Well, I Have a Message for You Madam Ji
+                  💌 Well, I Have a Message for You JIJA JII
                 </button>
               )}
             </div>
